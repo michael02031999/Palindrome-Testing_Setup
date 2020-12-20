0fb5c28cad56a230580eff46b2724c2b7179e8b7
Michael <michael.galan@uconn.edu>
2020-12-20 12:50:12 -0500

    12/20/20

---
+++ test/test.js
@@ -0,0 +1,12 @@
+let assert = require("assert");
+let Phrase = require("../index.js");
+
+describe("Phrase", function() {
+	// Phrase#palindrome
+	describe("#palindrome", function() {
+
+		it("should return a false for a non-palindrone", function() {
+			assert(true);
+		});
+	});
+});
